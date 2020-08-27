0395684dc7f804d2e573893422c9b97d474ba5f8
Charlie Zhu <goodgoodwish@gmail.com>
2020-08-26 22:10:51 -0700

    create app frame and model

--- job/views.py
+++ job/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
+from .models import Job
 
 # Create your views here.
 
 def home(request):
-    return render(request, "job/home.html")
+    jobs = Job.objects.all()
+    context = {"jobs": jobs}
+    return render(request, "job/home.html", context)
