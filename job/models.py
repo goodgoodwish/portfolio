968598423a9f27a8e960e995f1062fd3ff89fb10
Charlie Zhu <goodgoodwish@gmail.com>
2020-08-27 17:31:40 -0700

    Complete job details.

--- job/models.py
+++ job/models.py
@@ -5,3 +5,6 @@ from django.db import models
 class Job(models.Model):
     image = models.ImageField(upload_to="images/")  # pip install Pillow
     summary = models.CharField(max_length=200)
+
+    def __str__(self, ):
+        return self.summary
--- job/views.py
+++ job/views.py
@@ -1,9 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Job
 
-# Create your views here.
 
+# Create your views here.
 def home(request):
     jobs = Job.objects.all()
     context = {"jobs": jobs}
     return render(request, "job/home.html", context)
+
+def job(request, job_id):
+    # job = Job.objects.get(id=job_id)
+    job = get_object_or_404(Job, pk=job_id)
+    context = {"job": job}
+    return render(request, "job/job.html", context)
